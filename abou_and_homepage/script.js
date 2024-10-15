8459ebaec8b7625644b1fc14bab8cc43df0574fa
Johpaulcastro <143856220+Johnpaulcastro23@users.noreply.github.com>
2024-10-14 21:26:43 -0700

    Update script.js

--- abou_and_homepage/script.js
+++ abou_and_homepage/script.js
@@ -1,29 +1,43 @@
 function showAbout() {
-    document.getElementById("home-section").style.display = "none";
-    document.getElementById("about-section").style.display = "block";
-    document.getElementById("footer").style.display = "none"; // Hide footer
+    document.getElementById("home-section").style.display = "none"; 
+    document.getElementById("about-section").style.display = "block"; 
+    document.getElementById("footer").style.display = "none";
     updateActiveLink('about');
 }
 
 function showHome() {
-    document.getElementById("home-section").style.display = "flex"; // Change display to flex
-    document.getElementById("about-section").style.display = "none";
-    document.getElementById("footer").style.display = "block"; // Show footer
+    document.getElementById("home-section").style.display = "flex"; 
+    document.getElementById("about-section").style.display = "none"; 
+    document.getElementById("footer").style.display = "block";
     updateActiveLink('home');
 }
 
 function updateActiveLink(page) {
-    const homeLink = document.querySelector('nav ul li a[href="#"]');
-    const aboutLink = document.querySelector('nav ul li a[href="#"] + a');
-    const productLink = document.querySelector('nav ul li a[href="#"] + a + a');
+    const links = document.querySelectorAll('nav ul li a'); 
 
-    homeLink.classList.remove('active');
-    aboutLink.classList.remove('active');
-    productLink.classList.remove('active');
+    links.forEach(link => {
+        link.classList.remove('active');
+    });
 
     if (page === 'home') {
-        homeLink.classList.add('active');
+        links[0].classList.add('active');
     } else if (page === 'about') {
-        aboutLink.classList.add('active');
+        links[1].classList.add('active');
     }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1; 
+    }    
+    slides[slideIndex - 1].style.display = "block"; 
+    setTimeout(showSlides, 3000); 
 }
